a0f7702091436e7d8cd5644950bfa424de520f95
Ross Cohen <rosscohen1990@gmail.com>
2024-06-28 22:07:27 -0400

    Update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,10 +64,11 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
+      userName: username!,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
+
     };
 
     const newUser = await createUser(user);
